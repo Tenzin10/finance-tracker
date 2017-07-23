36420d27936d91b18616b98ba9555304bb3e638f
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-07-23 10:30:56 +0530

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :user_stocks, except: [:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
 end
